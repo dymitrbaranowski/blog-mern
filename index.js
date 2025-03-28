cbb173374276073b3fe7abf869981fbe462b4f02
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2025-03-28 12:02:56 +0200

    fox1

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
 import mongoose from "mongoose";
+import { validationResult } from "express-validator";
+
+import { registerValidation } from "./validations/auth.js";
+
+import UserModel from "./models/User.js";
 
 mongoose
-  .connect("mongodb+srv://admin:wwwwww@cluster0.43lrtqn.mongodb.net/")
+  // .connect("mongodb+srv://admin:wwwwww@cluster0.43lrtqn.mongodb.net/blog")
+  .connect("mongodb+srv://admin:wwwwww@cluster0.43lrtqn.mongodb.net/blog")
+  // .connect(
+  //   "mongodb+srv://admin:wwwwww@cluster0.43lrtqn.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0"
+  // )
   .then(() => console.log("DB is OK"))
-  .catch(() => console.log("DB error", err));
+  .catch((err) => console.log("DB error", err));
 
 const app = express();
 
@@ -13,8 +24,26 @@ const PORT = process.env.PORT || 4444;
 
 app.use(express.json());
 
-app.post("/auth/register", (req, res) => {
-  req;
+app.post("/auth/register", registerValidation, async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors.array());
+  }
+
+  const password = req.body.password;
+  const salt = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const doc = new UserModel({
+    email: req.body.email,
+    fullName: req.body.fullName,
+    avatarUrl: req.body.avatarUrl,
+    passwordHash,
+  });
+
+  const user = await doc.save();
+
+  res.json(user);
 });
 
 app.listen(PORT, (err) => {
